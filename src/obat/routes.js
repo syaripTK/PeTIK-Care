458b0de6556796bdc4a8a9abaf70dcbe6b58569a
Yann <yann@gmail.com>
2026-02-02 08:23:06 +0700

    nambahin search nama obat

--- src/obat/routes.js
+++ src/obat/routes.js
@@ -1,13 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 const uploadObat = require("../middlewares/uploadMiddleware.js");
-const cekUkuran = require("../middlewares/errorHandler.js")
-const { getAllObat, createObat, getObatById, updateObat, deleteObat } = require('./controllers.js');
+const cekUkuran = require("../middlewares/errorHandler.js");
+const {
+  getAllObat,
+  createObat,
+  getObatById,
+  updateObat,
+  deleteObat,
+  searchObat,
+} = require("./controllers.js");
 
-router.get('/', getAllObat);
-router.post('/tambah/',uploadObat.single("foto_obat"),cekUkuran, createObat);
-router.get('/cari/:id', getObatById);
-router.patch('/ubah/:id',uploadObat.single("foto_obat"), updateObat);
-router.delete('/hapus/:id', deleteObat);
+router.get("/", getAllObat);
+router.post("/tambah/", uploadObat.single("foto_obat"), cekUkuran, createObat);
+router.get("/cari/:id", getObatById);
+router.patch("/ubah/:id", uploadObat.single("foto_obat"), updateObat);
+router.delete("/hapus/:id", deleteObat);
+router.get("/search", searchObat);
 
 module.exports = router;
--- src/obat/services.js
+++ src/obat/services.js
@@ -1,36 +1,47 @@
-const {where} = require("sequelize");
+const { where, Op } = require("sequelize");
 const db = require("../db/models/index.js");
-const {Obat} = db;
+const { Obat } = db;
 
 const tampilObat = async () => {
-    return await Obat.findAll();
+  return await Obat.findAll();
 };
 
 const tambahObat = async (body) => {
-    return await Obat.create(body);
+  return await Obat.create(body);
 };
 
 const cariIdObat = async (id) => {
-    return await Obat.findByPk(id);
+  return await Obat.findByPk(id);
 };
 
 const ubahObat = async (id, body) => {
-    return Obat.update(body, {
-        where: { id }
-    });
+  return Obat.update(body, {
+    where: { id },
+  });
 };
 
 const hapusObat = async (id) => {
-    const result = await Obat.destroy({
-        where: { id }
-    });
-    return result;
-}
+  const result = await Obat.destroy({
+    where: { id },
+  });
+  return result;
+};
+
+const cariNamaObat = async (nama) => {
+  return await Obat.findAll({
+    where: {
+      nama_obat: {
+        [Op.like]: `%${nama}%`,
+      },
+    },
+  });
+};
 
 module.exports = {
-    tampilObat,
-    tambahObat,
-    cariIdObat,
-    ubahObat,
-    hapusObat
-}
+  tampilObat,
+  tambahObat,
+  cariIdObat,
+  ubahObat,
+  hapusObat,
+  cariNamaObat,
+};
