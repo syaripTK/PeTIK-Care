64324c9a92b0b7bca98fa761dc8d422061a0d8da
Ari <arisaprudin0005@gmail.com>
2026-02-03 08:58:21 +0700

    fixx goll

--- src/db/migrations/20260129035831-create-obat.js
+++ src/db/migrations/20260129035831-create-obat.js
@@ -1,44 +1,45 @@
-'use strict';
+"use strict";
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('obat', {
+    await queryInterface.createTable("obat", {
       id: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
       },
       nama_obat: {
         type: Sequelize.STRING(50),
-        allowNull: false
+        allowNull: false,
       },
       stok: {
         type: Sequelize.INTEGER,
-        allowNull: false
+        allowNull: false,
       },
       kategori: {
-        type: Sequelize.ENUM('obat-bebas', 'obat-terbatas', 'obat-keras', 'psikotropika'),
-        allowNull: false
+        type: Sequelize.ENUM(
+          "obat-bebas",
+          "obat-terbatas",
+          "obat-keras",
+          "psikotropika",
+        ),
+        allowNull: false,
       },
       foto_obat: {
-<<<<<<< HEAD
-        type: Sequelize.STRING
-=======
         type: Sequelize.STRING,
->>>>>>> main
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('obat');
-  }
+    await queryInterface.dropTable("obat");
+  },
 };
